55894042f0ba3a313e01541a396481d491963130
eliraybon <elr0615@gmail.com>
2019-11-09 18:03:10 -0500

    Setup React router

--- frontend/robotify.jsx
+++ frontend/robotify.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { signup, login, logout } from './actions/session_actions';
 import configureStore from './store/store';
+import Root from './components/root';
+
+// FOR TESTING
+import { signup, login, logout } from './actions/session_actions';
 
 
 document.addEventListener("DOMContentLoaded", () => {
   const store = configureStore();
 
+  //TESTING START
   window.getState = store.getState;
   window.dispatch = store.dispatch;
   window.signup = signup;
   window.login = login;
   window.logout = logout;
+  //TESTING END
 
   const root = document.getElementById("root");
-  ReactDOM.render(<h1>Welcome to Robotify</h1>, root);
+  ReactDOM.render(<Root store={ store }/>, root);
 });
