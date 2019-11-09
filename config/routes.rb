687177d5e62ebb88e34d9b7131862878f0b1e561
eliraybon <elr0615@gmail.com>
2019-11-09 16:15:21 -0500

    Implement Api::Users and Api::Sessions controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  
+  namespace :api, defaults: {format: :json} do
+    resources :users, only: [:create]
+    resource :session, only: [:create, :destroy]
+  end
 
   root to: 'static_pages#root'
 end
