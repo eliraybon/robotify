7011a7963944b5330ce4df613b869c8e03d5df60
eliraybon <elr0615@gmail.com>
2019-11-09 12:39:20 -0500

    Create Like model and migration

---
+++ db/migrate/20191109173809_create_likes.rb
@@ -0,0 +1,12 @@
+class CreateLikes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :likes do |t|
+      t.integer :user_id, null: false
+      t.integer :likeable_id, null: false
+      t.string :likeable_type, null: false
+
+      t.timestamps
+    end
+    add_index :likes, [:user_id, :likeable_id, :likeable_type], unique: true
+  end
+end
