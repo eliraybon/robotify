4c8662cab6e3c7c74cffe8829a66f6e11cb3a93c
eliraybon <elr0615@gmail.com>
2019-11-09 16:47:00 -0500

    Create session_reducer

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -4,10 +4,10 @@ export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
 export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
 
-const receiveCurrentUser = user => {
+const receiveCurrentUser = currentUser => {
   return {
     type: RECEIVE_CURRENT_USER,
-    user
+    currentUser
   };
 };
 
