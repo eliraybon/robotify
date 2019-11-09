3de6bc4a31c71f5c20a4bf8d5afaeb8d69e601d1
eliraybon <elr0615@gmail.com>
2019-11-09 15:54:31 -0500

    Add Application Controller methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def login(user)
+    @current_user = user
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def logout
+    current_user.reset_session_token!
+    session[:session_token] = nil
+  end
+
+  #I'm handling this on the front end... Do I need anything here?
+
+  # def require_logged_in
+  #   redirect_to new_session_url unless logged_in?
+  # end
 end
