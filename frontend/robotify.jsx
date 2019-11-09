2d17d75343cf459b3cb83a10300772d1418dc776
eliraybon <elr0615@gmail.com>
2019-11-09 16:39:49 -0500

    Test session_api_util methods

--- frontend/robotify.jsx
+++ frontend/robotify.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { signup, login, logout } from './util/session_api_util';
 
 document.addEventListener("DOMContentLoaded", () => {
+  window.signup = signup;
+  window.login = login;
+  window.logout = logout;
+
   const root = document.getElementById("root");
   ReactDOM.render(<h1>Welcome to Robotify</h1>, root);
 });
