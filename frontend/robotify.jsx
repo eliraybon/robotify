3eb572df9ceb7e5ab250827dbd048086e0f85e6e
eliraybon <elr0615@gmail.com>
2019-11-09 17:54:38 -0500

    Test initial redux state

--- frontend/robotify.jsx
+++ frontend/robotify.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { signup, login, logout } from './util/session_api_util';
+import { signup, login, logout } from './actions/session_actions';
+import configureStore from './store/store';
+
 
 document.addEventListener("DOMContentLoaded", () => {
+  const store = configureStore();
+
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   window.signup = signup;
   window.login = login;
   window.logout = logout;
